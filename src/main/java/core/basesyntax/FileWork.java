6b690d0d4484ae1804b4a855d34778a4635b1bfe
Oksana Miazina <oksana.myazina@gmail.com>
2024-02-21 13:39:16 +0200

    feat: filter all words start with w

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,59 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
 public class FileWork {
+    public static final String PREFIX_W = "w";
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        String fileContent = readContentFile(fileName);
+        String[] words = getAllWords(fileContent);
+
+        return filterWordsStartsWith(PREFIX_W, words);
+    }
+
+    private String readContentFile(String fileName) {
+        Path path = Paths.get(fileName);
+        StringBuilder fileContent = new StringBuilder();
+
+        try (BufferedReader reader = Files.newBufferedReader(path)) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                fileContent.append(line.toLowerCase()).append(" ");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file", e);
+        }
+
+        return fileContent.toString();
+    }
+
+    private String[] getAllWords(String content) {
+        return content.split("[^\\w-']");
+    }
+
+    private String[] filterWordsStartsWith(String prefix, String[] words) {
+        StringBuilder wordsAtWBuilder = new StringBuilder();
+        String[] wordsAtW;
+
+        for (String word : words) {
+            if (word.startsWith(prefix)) {
+                wordsAtWBuilder.append(word).append(" ");
+            }
+        }
+
+        if (wordsAtWBuilder.isEmpty()) {
+            wordsAtW = new String[0];
+        } else {
+            wordsAtW = wordsAtWBuilder.toString().split(" ");
+            Arrays.sort(wordsAtW);
+        }
+
+        return wordsAtW;
     }
 }
